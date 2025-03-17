c8920975334553bf779d73176aa0ef4bbd0fff3f
Isaias Batista <isaias@isaiastech.com.br>
2025-03-17 11:42:53 -0300

    Ajustando código

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Git</title>
+</head>
+<body>
+    <h1>Somente Testando Código</h1>
+
+    <p>Lorem ipsum dolor 
+        sit amet consectetur adipisicing elit. 
+        Saepe rerum rem animi, vitae porro inventore?
+    </p>
+
+    <h5>Lorem ipsum dolor sit amet.</h5>
+
+    <p>Testando repositorio git</p>
+    
+</body>
+</html>
